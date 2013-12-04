f4f6561926b3764dc92cbbf8d197f43f08c6f5bd
Giovanni Bassi <giggio@giggio.net>
2013-12-03 22:44:52 -0200

    updating npm reference

--- tools/install.ps1
+++ tools/install.ps1
@@ -1,4 +1,4 @@
 param($installPath, $toolsPath, $package, $project)
 
-$npmPath = (join-path (join-path $installPath "..") "Npm.js.1.3.15\tools\npm.cmd")
+$npmPath = (join-path (join-path $installPath "..") "Npm.js.1.3.15.2\tools\npm.cmd")
 & $npmPath install bower
