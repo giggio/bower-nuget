edfda1f221bf9374bc7facc1b46933c2d531686c
Giovanni Bassi <giggio@giggio.net>
2013-12-05 13:35:09 -0200

    updating npm, and making it use the latest version automatically, and save on npm install

--- tools/install.ps1
+++ tools/install.ps1
@@ -1,6 +1,6 @@
 param($installPath, $toolsPath, $package, $project)
 
 if (!(test-path (join-path $installPath "..\..\node_modules\bower\package.json"))) {
-  $npmPath = (join-path (join-path $installPath "..") "Npm.js.1.3.15.3\tools\npm.cmd")
-  & $npmPath install bower
+  $npmPath = (join-path $toolsPath "npm.cmd")
+  & $npmPath install bower --save-dev
 }
