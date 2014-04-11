391817ae46624d8a996b2f45c8ddcc8ca0df1461
Giovanni Bassi <giggio@giggio.net>
2014-04-11 18:11:16 -0300

    bump version to 1.3.2

--- tools/install.ps1
+++ tools/install.ps1
@@ -2,5 +2,5 @@ param($installPath, $toolsPath, $package, $project)
 
 if (!(test-path (join-path $installPath "..\..\node_modules\bower\package.json"))) {
   $npmPath = (join-path $toolsPath "npm.cmd")
-  & $npmPath install bower --save-dev
+  & $npmPath install bower@1.3.2 --save-dev
 }
